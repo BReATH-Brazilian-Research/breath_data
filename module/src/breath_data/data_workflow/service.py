96643dfaef2245a50172440c079601435010b3d7
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-11-07 19:56:25 -0300

    Inserção de dados do OpenSus

--- module/src/breath_data/data_workflow/service.py
+++ module/src/breath_data/data_workflow/service.py
@@ -3,6 +3,12 @@ from breath_api_interface.queue import Queue
 from breath_api_interface.service_interface import Service
 from breath_api_interface.request import Request, Response
 
+import pandas as pd
+from pandas.core import series
+
+import open_sus
+import ibge
+
 class DataWorflow(Service):
     def __init__(self, proxy:ServiceProxy, request_queue:Queue):
         '''DataWorflow constructor.
@@ -27,4 +33,80 @@ class DataWorflow(Service):
         request.send_response(response)
 
     def _load_open_sus_data(self, request:Request) -> Response:
-        ...
+        SINTOMA_DICT = {
+                    "FEBRE":"Febre", 
+                    "TOSSE": "Tosse", 
+                    "GARGANTA" : "Dor_de_garganta", 
+                    "DISPNEIA" : "Dispneia", 
+                    "MIALGIA" : "Mialgia", 
+                    "SATURACAO" : "Saturação_O2", 
+                    "DESC_RESP" : "Desconforto_Respiratório",
+                    "OUTRO_SIN" : "Outro_Sintoma",
+                    "PNEUMOPATI" :"Pneumopatia_crônica_associada",
+                    "CARDIOPATI" : "Cardiopatia_crônica_associada",
+                    "IMUNODEPRE" : "Imunodepressão_associada",
+                    "HEPATICA": "Doença_hepática_crônica",
+                    "NEUROLOGIC": "Doenca_neuorlógica",
+                    "RENAL" : "Doença_renal_crônica_associada",
+                    "SIND_DOWN" : "Síndrome_de_Down",
+                    "METABOLICA": "Doença_metabólica_crônica_associada",
+                    "PUERPERA" : "Puérpera",
+                    "OBESIDADE" : "Obesidade",
+                    "OUT_MORBI" : "Outra_morbidade_associada",
+                    "ANTIVIRAL" : "Usa_antiviral",
+                    "HOSPITAL" : "Hospitalização_por_Influenza",
+                    "RES_FLUA" : "Diagnóstico_para_Influzenza_A",
+                    "RES_FLUB" : "Diagnóstico_para_Influzenza_B",
+                    "RES_PARA1" : "Diagnóstico_para_Parainfluenza_1",
+                    "RES_PARA2" : "Diagnóstico_para_Parainfluenza_2",
+                    "RES_PARA3" : "Diagnóstico_para_Parainfluenza_3",
+                    "RES_ADNO" : "Diagnóstico_para_Adenovírus"
+                    }
+
+        # outros: EVOLUCAO -> Óbito, Alta        
+
+        tipos_sintoma = list( SINTOMA_DICT.values() ) 
+
+        for tipo_sintoma in tipos_sintoma:
+            request = Request(service_name="BDAcessPoint", operation_name="register_symptom_type")
+            request.request_info = {"symptom_name": tipo_sintoma}
+            self._proxy.send_request(request)
+
+
+        table_ibge : pd.DataFrame = ibge.load_csv()
+        for i in range(ibge.shape[0]):
+            linha = table_ibge.iloc[i]
+            uf = linha["UF"]
+            nome = linha["Nome_Município"]
+            cod = linha["Código Município Completo"]
+
+            request = Request(service_name="BDAcessPoint", operation_name="register_city")
+            request.request_info = {"uf":uf, "nome":nome, "cod":cod}
+            self._proxy.send_request(request)
+
+        table : pd.DataFrame = open_sus.loadcsv()
+
+        n_dado = table.shape[0]
+
+        for i in range(n_dado):
+            linha = table.iloc[i]
+
+            sexo = linha["CS_SEXO"]
+            cidade = linha["ID_MUNICIP"]
+
+            request = Request(service_name="BDAcessPoint", operation_name="register_patient")
+            request.request_info = {"sex":sexo}
+            response = self._proxy.send_request(request)
+            cod = response.response_data["patient"]["Código"]
+
+            data = linha["DT_NOTIFIC"]
+            dia = int(data[:2])
+            mes = int(data[3:5])
+            ano = int(data[6:])
+
+            for sintoma_key in tipo_sintoma:
+                if linha["sintoma_key"] == 1:
+                    request = Request(service_name="BDAcessPoint", operation_name="register_symptom")
+                    request.request_info = {"year":ano, "month":mes, "day": dia, "city": cidade,
+                                            "symptom_name": tipo_sintoma[sintoma_key], "patient_id":cod}
+                    self._proxy.send_request(request)
