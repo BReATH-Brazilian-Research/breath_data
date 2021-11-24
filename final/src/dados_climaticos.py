9175ebd524acef31950a96237aaba0a12b87bccb
J <joao.bonucci@conpec.com.br>
2021-11-24 04:07:54 -0300

    temp commit

--- final/src/dados_climaticos.py
+++ final/src/dados_climaticos.py
@@ -7,14 +7,19 @@ from relational_querier import RelationalQuerier
 #from graph_querier import GraphQuerier
 
 # os dados climaticos brutos foram baixados diretamente do link: https://www.kaggle.com/PROPPG-PPG/hourly-weather-surface-brazil-southeast-region
-abbreviation = ['date','precipitacao','pressao_at_max', 'pressao_at_min', 'radiacao', 'temp_max','temp_min','umidade','max_vent','velocidade_vent','region','state','station','lat','lon','elvt']
+abbreviation = ['date', 'station','precipitacao','pressao_at_max', 'pressao_at_min', 'radiacao', 'temp_max','temp_min','umidade','max_vent','velocidade_vent','region','state','lat','lon','elvt']
 
 db = RelationalQuerier()
 
+def d(x):
+	y = x.split('-')
+	y.reverse()
+	return '/'.join(y)
+	
 def format_csv(file_name):
 	f1=open(file_name,"r+")
 	input=f1.read()
-	input=input.replace(';',',')
+	input=input.replace('-','/')
 	f2=open(file_name,"w+")
 	f2.write(input)
 	f1.close()
@@ -26,32 +31,50 @@ def clean(file_name):
 	! process data
 	! clean na
 	"""
-	format_csv(file_name)
-
+	#format_csv(file_name)
+	print('formatando dados')
 	df = pd.read_csv(file_name)
 
+	print('removendo valores nulos')
 	for i in df.columns:
 		df = df.loc[df[i] != -9999]
+	print('removeu valores nulos')
 
-	preci = df.groupby(['Data'])['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].sum().reset_index()
-	max_pres = df.groupby(['Data'])['PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)'].max().reset_index()
-	min_pres = df.groupby(['Data'])['PRESSÃO ATMOSFERICA MIN. NA HORA ANT. (AUT) (mB)'].min().reset_index()
-	rad = df.groupby(['Data'])['RADIACAO GLOBAL (Kj/m²)'].mean().reset_index()
-	temp_max = df.groupby(['Data'])['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].max().reset_index()
-	temp_min = df.groupby(['Data'])['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].min().reset_index()
-	umidade = df.groupby(['Data'])['UMIDADE RELATIVA DO AR, HORARIA (%)'].mean().reset_index()
-	max_vent = df.groupby(['Data'])['VENTO, RAJADA MAXIMA (m/s)'].max().reset_index()
-	vel_vent = df.groupby(['Data'])['VENTO, VELOCIDADE HORARIA (m/s)'].mean().reset_index()
-	region = df.groupby(['Data'])['region'].min().reset_index()
-	state = df.groupby(['Data'])['state'].min().reset_index()
-	station = df.groupby(['Data'])['station'].min().reset_index()
-	latitude = df.groupby(['Data'])['latitude'].min().reset_index()
-	longitude = df.groupby(['Data'])['longitude'].min().reset_index()
-	height = df.groupby(['Data'])['height'].min().reset_index()
+	preci = df.groupby(['Data', 'station'])['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].sum().reset_index()
+	max_pres = df.groupby(['Data', 'station'])['PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)'].mean().reset_index()
+	min_pres = df.groupby(['Data', 'station'])['PRESSÃO ATMOSFERICA MIN. NA HORA ANT. (AUT) (mB)'].mean().reset_index()
+	rad = df.groupby(['Data', 'station'])['RADIACAO GLOBAL (Kj/m²)'].mean().reset_index()
+	temp_max = df.groupby(['Data', 'station'])['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].max().reset_index()
+	temp_min = df.groupby(['Data', 'station'])['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].min().reset_index()
+	umidade = df.groupby(['Data', 'station'])['UMIDADE RELATIVA DO AR, HORARIA (%)'].mean().reset_index()
+	max_vent = df.groupby(['Data', 'station'])['VENTO, RAJADA MAXIMA (m/s)'].max().reset_index()
+	vel_vent = df.groupby(['Data', 'station'])['VENTO, VELOCIDADE HORARIA (m/s)'].mean().reset_index()
+	region = df.groupby(['Data', 'station'])['region'].first().reset_index()
+	state = df.groupby(['Data', 'station'])['state'].first().reset_index()
+	latitude = df.groupby(['Data', 'station'])['latitude'].first().reset_index()
+	longitude = df.groupby(['Data', 'station'])['longitude'].first().reset_index()
+	height = df.groupby(['Data', 'station'])['height'].first().reset_index()
 
-	clmns = [preci, max_pres, min_pres, rad, temp_max, temp_min, umidade, max_vent, vel_vent, region, state, station, latitude, longitude, height]
-	df_final = reduce(lambda left,right: pd.merge(left,right,on='Data'), clmns)
+	max_pres.drop(columns=['Data', 'station'], inplace=True)
+	min_pres.drop(columns=['Data', 'station'], inplace=True)
+	rad.drop(columns=['Data', 'station'], inplace=True)
+	temp_max.drop(columns=['Data', 'station'], inplace=True)
+	temp_min.drop(columns=['Data', 'station'], inplace=True)
+	umidade.drop(columns=['Data', 'station'], inplace=True)
+	max_vent.drop(columns=['Data', 'station'], inplace=True)
+	vel_vent.drop(columns=['Data', 'station'], inplace=True)
+	region.drop(columns=['Data', 'station'], inplace=True)
+	state.drop(columns=['Data', 'station'], inplace=True)
+	latitude.drop(columns=['Data', 'station'], inplace=True)
+	longitude.drop(columns=['Data', 'station'], inplace=True)
+	height.drop(columns=['Data', 'station'], inplace=True)
+
+	print('renomeando colunas')
+	clmns = [preci, max_pres, min_pres, rad, temp_max, temp_min, umidade, max_vent, vel_vent, region, state, latitude, longitude, height]
+	df_final = pd.concat(clmns, axis=1,join="outer",ignore_index=False,keys=None,levels=None,names=None,verify_integrity=False,copy=False)
 	df_final.columns = abbreviation
+
+	df_final['date'] = df_final['date'].map(lambda x: d(x))
 
 	new_file_name = file_name.split('.')[0]+'_clean.'+file_name.split('.')[1]
 	print(new_file_name)
@@ -77,7 +100,7 @@ def add_data_graph(df = None, csv = None):
 	db._close()
 
 def add_data_relational(db, df = None, csv = None):
-
+	print('adicionando dados')
 	if df is None and csv is not None:
 		df = pd.read_csv(csv)
 		
@@ -89,16 +112,16 @@ def add_data_relational(db, df = None, csv = None):
 		print(i)
 		query = """
 		INSERT INTO Clima
-		(date,precipitacao,pressao_at_max, pressao_at_min, radiacao,
+		(date,station,precipitacao,pressao_at_max, pressao_at_min, radiacao,
 		temp_max,temp_min,umidade,max_vent,velocidade_vent,region,
-		state,station,lat,lon,elvt)
+		state,lat,lon,elvt)
 		VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
 
 		result = db.query(query, data)
 	db.commit()
 
 def add_data_relational_stations(db, df = None, csv = None):
-
+	print('adicionando estacoes meteo')
 	if df is None and csv is not None:
 		df = pd.read_csv(csv)
 	
@@ -121,13 +144,14 @@ if __name__ == "__main__":
 	print(files)
 	# para cada um dos arquivos
 	for file in files:
+		print('iniciando processamento')
 		name = file.split('/')[-1].split('.')[0]
 		print("processando arquivo:",name)
 		if name in ["central_west", "north", "northeast", "south", "southeast"]:
 			print('arquivo nao formatado, iniciando limpeza de arquivo', name)
 			df, new_file_name = clean(file)
 			print(f'Dados tratados estao no arquivo {new_file_name} na mesma pasta dos originais')
-
+	
 		# caso contrario apenas adicione no banco de dados
 		elif name in ["central_west_clean", "north_clean", "northeast_clean", "south_clean", "southeast_clean"]:
 			print('arquivo formatado, iniciando insercao no banco de dados')
@@ -139,8 +163,7 @@ if __name__ == "__main__":
 
 		print('processamento e insercao concluidos\n')
 	# veja se a insercao deu certo
-	query = 'SELECT * FROM Clima;'
-	result = db.query(query)
-	print('result:', result)
-	#add_data_graph(df)
+	#query = 'SELECT * FROM Clima;'
+	#result = db.query(query)
+	#print('result:', result)
 	db._close()
